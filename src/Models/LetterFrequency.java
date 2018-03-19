e09fbac0ed2d206f594e8d4f1940466f60adb8dc
al3xhh <al3xhh1995@gmail.com>
2018-03-19 15:13:44 +0100

    Added letter frequency.

---
+++ src/Models/LetterFrequency.java
@@ -0,0 +1,31 @@
+package Models;
+
+public class LetterFrequency {
+	
+	public static final double A = 8.167;
+	public static final double B = 1.492;
+	public static final double C = 2.782;
+	public static final double D = 4.253;
+	public static final double E = 12.702;
+	public static final double F = 2.228;
+	public static final double G = 2.015;
+	public static final double H = 6.094;
+	public static final double I = 6.966;
+	public static final double J = 0.153;
+	public static final double K = 0.772;
+	public static final double L = 4.025;
+	public static final double M = 2.406;
+	public static final double N = 6.749;
+	public static final double O = 7.507;
+	public static final double P = 1.929;
+	public static final double Q = 0.095;
+	public static final double R = 5.987;
+	public static final double S = 6.327;
+	public static final double T = 9.056;
+	public static final double U = 2.758;
+	public static final double V = 0.978;
+	public static final double W = 2.360;
+	public static final double X = 0.150;
+	public static final double Y = 1.974;
+	public static final double Z = 0.074;
+}
